3322444b89f2956bb21e2de1d22e5dffd74729ac
Dominik Simon <dosimion@edu.aau.at>
2018-03-28 19:19:39 +0200

    changed GameObject

--- core/src/at/uni/objects/GameObject.java
+++ core/src/at/uni/objects/GameObject.java
@@ -2,15 +2,14 @@ package at.uni.objects;
 
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
 
 public abstract class GameObject {
 
     protected Vector2 position;
     protected Rectangle bounds;
 
-    public abstract Body getBody();
-
+    public abstract void load(World world);
 
     public Vector2 getPosition() {
         return position;
