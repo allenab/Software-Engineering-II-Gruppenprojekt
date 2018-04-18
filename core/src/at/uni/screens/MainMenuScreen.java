5b688f3257a50f3848a6a64b371b76a2708d1e9d
KatharinaKofler <katharinako@edu.aau.at>
2018-04-18 16:48:46 +0200

    Menü Prototyp neu

--- core/src/at/uni/screens/MainMenuScreen.java
+++ core/src/at/uni/screens/MainMenuScreen.java
@@ -5,19 +5,22 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 import at.uni.Application;
+import at.uni.handlers.GameScreenManager;
 import at.uni.utils.InputData;
 
 public class MainMenuScreen extends AbstractScreen {
 
-    private BitmapFont font;
     private OrthographicCamera camera;
 
     public MainMenuScreen(Application application) {
         super(application);
 
-        font = new BitmapFont();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Application.VIEWPORT_WIDTH, Application.VIEWPORT_HEIGHT);
 
@@ -31,56 +34,52 @@ public class MainMenuScreen extends AbstractScreen {
 
         // creates the pack for the buttons, its skin, the font used on the buttons, and the stage(screen)
 
-        atlas = new TextureAtlas("buttons.pack");
-        skin = new Skin();
-        skin.addRegions(atlas);
-        font = new BitmapFont(Gdx.files.internal("font.fnt"), false);
-
         //creates the play button with the text, its position and the size
-
-        btnPlay = new TextButton("Play", style);
+        Skin skin = new Skin(Gdx.files.internal("data/uiskin.json"));
+        TextButton btnPlay = new TextButton("Play", skin);
         btnPlay.setSize(80, 50);
         btnPlay.setPosition(100, 200);
 
         btnPlay.addListener(new InputListener() {
-
-             if(InputData.isTouched(0, new Rectangle(100, Application.VIEWPORT_HEIGHT - 100, 50 , 20)){
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 System.out.println("PLAY");
-            })
-
-
+                super.touchUp(event, x, y, pointer, button);
+            }
         });
+
+        stage.addActor(btnPlay);
 
         //creates the exit button with the text, its position and the size
 
-        btnExit = new TextButton("Exit", style);
+        TextButton btnExit = new TextButton("Exit", skin);
         btnExit.setSize(80,50);
         btnExit.setPosition(300, 100);
 
         btnExit.addListener(new InputListener() {
-
-            if(InputData.isPressed(1, new Rectangle(100,Application.VIEWPORT_HEIGHT - 100,50,20))){
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 System.out.println("EXIT");
+                super.touchUp(event, x, y, pointer, button);
             }
-
         });
+
+        stage.addActor(btnExit);
 
         //creates the setting button with the text, its position and the size
 
-        btnExit = new TextButton("Settings", style);
-        btnExit.setSize(80,50);
-        btnExit.setPosition(500, 100);
+        TextButton btnSettings = new TextButton("Settings", skin);
+        btnSettings.setSize(80,50);
+        btnSettings.setPosition(500, 100);
 
-        btnExit.addListener(new InputListener() {
-
-            if(InputData.isPressed(1, new Rectangle(100,Application.VIEWPORT_HEIGHT - 100,50,20))){
+        btnSettings.addListener(new InputListener() {
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
                 System.out.println("SETTINGS");
+                super.touchUp(event, x, y, pointer, button);
             }
-
         });
 
-        stage.addActor(btnPlay);
-        stage.addActor(btnExit);
         stage.addActor(btnSettings);
 
         super.show();
@@ -89,7 +88,6 @@ public class MainMenuScreen extends AbstractScreen {
     @Override
     public void handleInput() {
 
-        }
     }
 
     @Override
@@ -99,24 +97,11 @@ public class MainMenuScreen extends AbstractScreen {
 
     @Override
     public void render(SpriteBatch sb) {
-        sb.begin();
-        font.draw(sb, "GAME MENU", 100, 100);
-        sb.end();
-
-        Gdx. gl .glClearColor (1, 1, 1, 1);
-        Gdx. gl .glClear (GL10. GL_COLOR_BUFFER_BIT );
-
-        if (Gdx. Eingabe .isKeyPressed (Input.Keys. LEFT )) {
-            if (Gdx. Eingabe .isKeyPressed (Input.Keys. CONTROL_LEFT ))
-                //In Liste speichern
-                //Wenn kein Name vorhanden kann man das Spiel nicht starten und es soll eine Fehlermeldung:'
-                //Bitte Namen eingeben!! ausgeben.
-            }
 
     }
 
     @Override
     public void dispose() {
-        font.dispose();
+
     }
 }
